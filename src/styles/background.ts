49df7de4b056d66447dee74ea51d6597ef57be84
gennadii <gennadiixd@gmail.com>
2019-10-21 11:54:13 +0300

    refactor

--- src/styles/background.ts
+++ src/styles/background.ts
@@ -1,17 +1,3 @@
-export const getBackgroundApp = (stateOfWeather: string, location?: any): string => {
-    if (location !== '/today'){
-        return `#F9F9F9`
-    }
-    
-    if ((/Cloud/).test(stateOfWeather) || (/Rain/).test(stateOfWeather)) {
-        return `#EAEAEA`;
-    } else if ((/Clear/).test(stateOfWeather) || (/Light/).test(stateOfWeather) || (/Showers/).test(stateOfWeather)) {
-        return `#FFF7E0`;
-    } else {
-        return `#F9F9F9`
-    }
-}
-
 export const getBackgroundCard = (stateOfWeather: string): string => {
     if ((/Clear/).test(stateOfWeather) || (/Light/).test(stateOfWeather) || (/Showers/).test(stateOfWeather) || (/Light Cloud/).test(stateOfWeather)) {
         return `#FFF7E0`;
@@ -20,4 +6,11 @@ export const getBackgroundCard = (stateOfWeather: string): string => {
     } else {
         return `#F9F9F9`
     }
+}
+export const getBackgroundApp = (stateOfWeather: string, location?: any): string => {
+    if (location !== '/today'){
+        return `#F9F9F9`
+    }
+    
+    return getBackgroundCard(stateOfWeather)
 }
